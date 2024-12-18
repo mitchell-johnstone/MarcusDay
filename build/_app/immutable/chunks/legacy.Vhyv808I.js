8d074ee88e00ac57d5fcdb18a429657a4e0b7182
Mitchell Johnstone <mitchell.johnstone@directsupply.com>
2024-12-18 14:02:52 -0600

    add the latest buildt' t'

---
+++ build/_app/immutable/chunks/legacy.Vhyv808I.js
@@ -0,0 +1,1 @@
+import{e}from"./runtime.Rs9OVjae.js";e();
---
+++ build/_app/immutable/nodes/2.IbDjwsZ8.js
@@ -0,0 +1,1 @@
+import{a as o,t as a}from"../chunks/disclose-version.BJ6ODgea.js";import"../chunks/legacy.Vhyv808I.js";import{n as p}from"../chunks/runtime.Rs9OVjae.js";var r=a('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=r();p(2),o(t,e)}export{s as component};
